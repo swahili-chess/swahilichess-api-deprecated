82280750aa2a50ef3c148f8532c61a099fd390e4
Hopertz <lugano.ngulwa@gmail.com>
2023-08-22 14:07:42 -0400

    Fix bug, forgot to change name to lichess_id

--- src/resources/lichess/lichess.controller.js
+++ src/resources/lichess/lichess.controller.js
@@ -3,7 +3,7 @@ import getUserNames from "../../utils/db.js"
 
 export const getTopTen = async (req, res) => {
     let users = await getUserNames()
-    const usersText = users.map(item => item.name).join(',');
+    const usersText = users.map(item => item.lichess_id).join(',');
     const response= await getTopTenHelper(usersText)
     res.status(200).json(response)
 }
