636683fb9a96f02779d28e5acc9b7bbbd9cf144d
Hopertz <lugano.ngulwa@gmail.com>
2023-08-22 14:57:14 -0400

    Show username instead of lichess_id

--- src/resources/lichess/lichess.controller.js
+++ src/resources/lichess/lichess.controller.js
@@ -2,9 +2,9 @@ import getTopTenHelper from "../../utils/utils.js"
 import getUserNames from "../../utils/db.js"
 
 export const getTopTen = async (req, res) => {
-    let users = await getUserNames()
-    const usersText = users.map(item => item.lichess_id).join(',');
-    const response= await getTopTenHelper(usersText)
+    let results = await getUserNames()
+    const lichess_ids = results.map(item => item.lichess_id).join(',');
+    const response= await getTopTenHelper(lichess_ids)
     res.status(200).json(response)
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
 import fetch from "node-fetch";
 const summary = { rapid: {}, blitz: {} };
 
-export default async function getTopTenHelper(usernames) {
+export default async function getTopTenHelper(lichess_ids) {
   const response = await fetch("https://lichess.org/api/users", {
     method: "post",
-    body: usernames,
+    body: lichess_ids,
     headers: { "Content-Type": "text/plain" },
   });
   const userObj = await response.json();
@@ -14,8 +14,8 @@ export default async function getTopTenHelper(usernames) {
         continue;
       }
     }
-    summary["rapid"][user["id"]] = user["perfs"]["rapid"]["rating"];
-    summary["blitz"][user["id"]] = user["perfs"]["blitz"]["rating"];
+    summary["rapid"][user["username"]] = user["perfs"]["rapid"]["rating"];
+    summary["blitz"][user["username"]] = user["perfs"]["blitz"]["rating"];
   }
 
   const sortedSlicedRapid = sliceDict(sortDictByValue(summary["rapid"]), 0, 20);
